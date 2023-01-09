c9a2a559f4d313a762cc92efd31844647085ff1b
Marcin Skic <marcinskicoficjal@gmail.com>
2023-01-09 08:16:33 +0100

    Create numbers generator

--- main.c
+++ main.c
@@ -3,15 +3,49 @@
 #include <stdlib.h>
 
 int cursorX = 0, cursorY = 0;
+int visuals[8][8];
 
-enum fieldFlags {isDiscovered = 0b001, isBomb = 0b010, isFlagged = 0b100};
+enum fieldFlags {empty = 0,one = 1,isDiscovered = 0b0010000, isBomb = 0b0100000, isFlagged = 0b1000000};
 
 int minesNumber = 10;
 int rows = 8;
 int cols = 8;
 
 enum fieldFlags fields[8][8];   //rows, cols
-int visuals[8][8];
+
+
+
+void generateNumbers(int row,int col){
+    if(row != 0){
+        fields[row-1][col]++;   //TOP
+
+        if(col != 0){
+            fields[row-1][col-1]++; //TOP LEFT
+        }
+        if(col < cols-1){
+            fields[row-1][col+1]++; //TOP RIGHT
+        }
+    }
+
+    if(col != 0){
+        fields[row][col-1]++;   //LEFT
+    }
+    if(col < cols-1){
+        fields[row][col+1]++;   //RIGHT
+    }
+
+    if(row < rows-1){
+        fields[row+1][col]++;   //BOTTOM
+
+        if(col != 0){
+            fields[row+1][col-1]++; //BOTTOM LEFT
+        }
+        if(col < cols-1){
+            fields[row+1][col+1]++; //BOTTOM RIGHT
+        }
+    }
+
+}
 
 void generateMap(){
 
@@ -43,14 +77,18 @@ void generateMap(){
     for(int i = 0; i < minesNumber; i++){
         int minePos = selectedMines[i];
         fields[minePos/cols][minePos%cols] = isBomb;
+        generateNumbers(minePos/cols,minePos%cols);
     }
 }
+
 
 void visualizeMap(){
     for (int i = 0; i < rows; i++){
         for(int x = 0; x < cols; x++){
             if(fields[i][x] & isBomb){
                 printf("B");
+            } else if(fields[i][x] > 0){
+                printf("%d",(int)fields[i][x]);
             } else {
                 printf("_");
             }
